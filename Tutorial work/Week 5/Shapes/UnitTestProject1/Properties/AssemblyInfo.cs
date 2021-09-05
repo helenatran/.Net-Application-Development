cffb6b2778c8da7eafb1c629acd01eeec3e8e177
helenatran <helena.e.tran@student.uts.edu.au>
2021-09-05 15:13:23 +1000

    Week5-Unit tests

---
+++ Tutorial work/Week 5/Shapes/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0cb1a3ec-f230-4d8e-981d-87e331e13023")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tutorial work/Week 5/Shapes/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week5LabProgramQuestion;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCircle()
+        {
+            var circle = new Shapes(4.0);
+            Assert.AreEqual(circle.CircleArea(), 50.272);
+        }
+
+        [TestMethod]
+        public void testSquare()
+        {
+            var square = new Shapes(5.0, 4.0);
+            Assert.AreEqual(square.RectangleArea(), 20.0);
+        }
+    }
+}
