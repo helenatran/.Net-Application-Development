294b40eafc6ba1699e105a07d52990347d92d7e5
helenatran <helena.e.tran@student.uts.edu.au>
2021-09-14 16:33:26 +1000

    Week7-WindowApp using Checkboxes to create a "Student Restaurant" app

---
+++ Tutorial work/Week 7/Week7Program1/Week7Program1/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Week8Program1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.menuGrpBox = new System.Windows.Forms.GroupBox();
+            this.drinksPrice = new System.Windows.Forms.Label();
+            this.pizzaPrice = new System.Windows.Forms.Label();
+            this.sushiPrice = new System.Windows.Forms.Label();
+            this.cerealPrice = new System.Windows.Forms.Label();
+            this.bananaBreadPrice = new System.Windows.Forms.Label();
+            this.juicePrice = new System.Windows.Forms.Label();
+            this.teaCoffeePrice = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.itemsLabel = new System.Windows.Forms.Label();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.menuGrpBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(112, 29);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(289, 36);
+            this.title.TabIndex = 0;
+            this.title.Text = "Student Restaurant";
+            // 
+            // menuGrpBox
+            // 
+            this.menuGrpBox.Controls.Add(this.drinksPrice);
+            this.menuGrpBox.Controls.Add(this.pizzaPrice);
+            this.menuGrpBox.Controls.Add(this.sushiPrice);
+            this.menuGrpBox.Controls.Add(this.cerealPrice);
+            this.menuGrpBox.Controls.Add(this.bananaBreadPrice);
+            this.menuGrpBox.Controls.Add(this.juicePrice);
+            this.menuGrpBox.Controls.Add(this.teaCoffeePrice);
+            this.menuGrpBox.Controls.Add(this.checkBox1);
+            this.menuGrpBox.Controls.Add(this.checkBox2);
+            this.menuGrpBox.Controls.Add(this.checkBox3);
+            this.menuGrpBox.Controls.Add(this.checkBox4);
+            this.menuGrpBox.Controls.Add(this.checkBox5);
+            this.menuGrpBox.Controls.Add(this.checkBox6);
+            this.menuGrpBox.Controls.Add(this.checkBox7);
+            this.menuGrpBox.Controls.Add(this.priceLabel);
+            this.menuGrpBox.Controls.Add(this.itemsLabel);
+            this.menuGrpBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuGrpBox.Location = new System.Drawing.Point(31, 122);
+            this.menuGrpBox.Name = "menuGrpBox";
+            this.menuGrpBox.Size = new System.Drawing.Size(456, 604);
+            this.menuGrpBox.TabIndex = 1;
+            this.menuGrpBox.TabStop = false;
+            this.menuGrpBox.Text = "Menu";
+            // 
+            // drinksPrice
+            // 
+            this.drinksPrice.AutoSize = true;
+            this.drinksPrice.Location = new System.Drawing.Point(325, 523);
+            this.drinksPrice.Name = "drinksPrice";
+            this.drinksPrice.Size = new System.Drawing.Size(58, 26);
+            this.drinksPrice.TabIndex = 15;
+            this.drinksPrice.Text = "3.50";
+            // 
+            // pizzaPrice
+            // 
+            this.pizzaPrice.AutoSize = true;
+            this.pizzaPrice.Location = new System.Drawing.Point(325, 453);
+            this.pizzaPrice.Name = "pizzaPrice";
+            this.pizzaPrice.Size = new System.Drawing.Size(58, 26);
+            this.pizzaPrice.TabIndex = 14;
+            this.pizzaPrice.Text = "9.00";
+            // 
+            // sushiPrice
+            // 
+            this.sushiPrice.AutoSize = true;
+            this.sushiPrice.Location = new System.Drawing.Point(325, 383);
+            this.sushiPrice.Name = "sushiPrice";
+            this.sushiPrice.Size = new System.Drawing.Size(58, 26);
+            this.sushiPrice.TabIndex = 13;
+            this.sushiPrice.Text = "3.00";
+            // 
+            // cerealPrice
+            // 
+            this.cerealPrice.AutoSize = true;
+            this.cerealPrice.Location = new System.Drawing.Point(325, 318);
+            this.cerealPrice.Name = "cerealPrice";
+            this.cerealPrice.Size = new System.Drawing.Size(58, 26);
+            this.cerealPrice.TabIndex = 12;
+            this.cerealPrice.Text = "5.50";
+            // 
+            // bananaBreadPrice
+            // 
+            this.bananaBreadPrice.AutoSize = true;
+            this.bananaBreadPrice.Location = new System.Drawing.Point(325, 257);
+            this.bananaBreadPrice.Name = "bananaBreadPrice";
+            this.bananaBreadPrice.Size = new System.Drawing.Size(58, 26);
+            this.bananaBreadPrice.TabIndex = 11;
+            this.bananaBreadPrice.Text = "2.50";
+            // 
+            // juicePrice
+            // 
+            this.juicePrice.AutoSize = true;
+            this.juicePrice.Location = new System.Drawing.Point(325, 195);
+            this.juicePrice.Name = "juicePrice";
+            this.juicePrice.Size = new System.Drawing.Size(58, 26);
+            this.juicePrice.TabIndex = 10;
+            this.juicePrice.Text = "3.50";
+            // 
+            // teaCoffeePrice
+            // 
+            this.teaCoffeePrice.AutoSize = true;
+            this.teaCoffeePrice.Location = new System.Drawing.Point(325, 137);
+            this.teaCoffeePrice.Name = "teaCoffeePrice";
+            this.teaCoffeePrice.Size = new System.Drawing.Size(58, 26);
+            this.teaCoffeePrice.TabIndex = 9;
+            this.teaCoffeePrice.Text = "4.80";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(38, 133);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(154, 30);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Tea/Coffee";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(38, 191);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(94, 30);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Juice";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(38, 253);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(189, 30);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Banana Bread";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(38, 522);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(106, 30);
+            this.checkBox7.TabIndex = 5;
+            this.checkBox7.Text = "Drinks";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(38, 452);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(97, 30);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.Text = "Pizza";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(38, 382);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(98, 30);
+            this.checkBox5.TabIndex = 7;
+            this.checkBox5.Text = "Sushi";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(38, 314);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(108, 30);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Cereal";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(308, 80);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(87, 26);
+            this.priceLabel.TabIndex = 1;
+            this.priceLabel.Text = "Price $";
+            // 
+            // itemsLabel
+            // 
+            this.itemsLabel.AutoSize = true;
+            this.itemsLabel.Location = new System.Drawing.Point(69, 80);
+            this.itemsLabel.Name = "itemsLabel";
+            this.itemsLabel.Size = new System.Drawing.Size(71, 26);
+            this.itemsLabel.TabIndex = 0;
+            this.itemsLabel.Text = "Items";
+            // 
+            // orderButton
+            // 
+            this.orderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderButton.Location = new System.Drawing.Point(44, 754);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(199, 50);
+            this.orderButton.TabIndex = 2;
+            this.orderButton.Text = "Order";
+            this.orderButton.UseVisualStyleBackColor = true;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(275, 754);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(199, 50);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 829);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.orderButton);
+            this.Controls.Add(this.menuGrpBox);
+            this.Controls.Add(this.title);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuGrpBox.ResumeLayout(false);
+            this.menuGrpBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.GroupBox menuGrpBox;
+        private System.Windows.Forms.Label itemsLabel;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label drinksPrice;
+        private System.Windows.Forms.Label pizzaPrice;
+        private System.Windows.Forms.Label sushiPrice;
+        private System.Windows.Forms.Label cerealPrice;
+        private System.Windows.Forms.Label bananaBreadPrice;
+        private System.Windows.Forms.Label juicePrice;
+        private System.Windows.Forms.Label teaCoffeePrice;
+    }
+}
+
---
+++ Tutorial work/Week 7/Week7Program1/Week7Program1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Week8Program1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        // Method to create the payment receipt 
+        public string GetReceipt()
+        {
+            double totalPrice = 0.0;
+            string receipt = "Student Restaurant \n\nYour Order Details: \n\n";
+
+            // Create a label array for food item prices and assigned each price item to an element
+            Label[] price = new Label[7];
+            price[0] = teaCoffeePrice;
+            price[1] = juicePrice;
+            price[2] = bananaBreadPrice;
+            price[3] = cerealPrice;
+            price[4] = sushiPrice;
+            price[5] = pizzaPrice;
+            price[6] = drinksPrice;
+
+            int count = 0;
+            // Process each selected food item and calculate the total price
+            foreach (CheckBox c in menuGrpBox.Controls.OfType<CheckBox>())
+            {
+                if (c.Checked == true)
+                {
+                    receipt = receipt + c.Text + ": $" + price[count].Text + "\n";
+                    totalPrice += Convert.ToDouble(price[count].Text);
+                }
+                count++;
+            }
+
+            receipt = receipt + "Total Price: $" + totalPrice;
+            return receipt;
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(GetReceipt(), "Receipt");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
