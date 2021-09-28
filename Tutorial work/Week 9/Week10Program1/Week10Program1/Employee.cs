9d443cf179bc6e5b2fe2956886bdfe4bd9932ca3
helenatran <helena.e.tran@student.uts.edu.au>
2021-09-28 21:40:25 +1000

    Week 9 Tutorial: Collections

---
+++ Tutorial work/Week 9/Week10Program1/Week10Program1/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Week10Program1
+{
+    class Employee : IComparable<Employee>
+    {
+        private string firstName, lastName;
+        private double hourlyRate, workHours;
+        private int employeeId;
+
+        // Employee Constructor
+        public Employee() { }
+
+        // Method to extract individual fields from employee details line
+        public void LoadEmployee(string fileLine)
+        {
+            char[] delimiterChars = { ',', ' ' };
+            string[] employeeInfo = fileLine.Split(delimiterChars, System.StringSplitOptions.RemoveEmptyEntries);
+
+            // Assign values to respective properties
+            firstName = employeeInfo[0];
+            lastName = employeeInfo[1];
+            hourlyRate = Convert.ToDouble(employeeInfo[2]);
+            employeeId = Convert.ToInt32(employeeInfo[3]);
+            workHours = Convert.ToDouble(employeeInfo[4]);
+        }
+
+        // Method to calculate the weekly salary of an Employee
+        public double GetWeeklySalary()
+        {
+            return workHours * hourlyRate;
+        }
+
+        // Overide the ToString() method to display the employee details
+        public override string ToString()
+        {
+            return firstName + " " + lastName + " ID#: " + employeeId + " Weekly Income: " + GetWeeklySalary();
+        }
+
+        // Method to compare 2 employees
+        public int CompareTo(Employee other)
+        {
+            if (employeeId < other.employeeId) return -1;
+            else if (employeeId == other.employeeId) return 1;
+            else return 0;
+        }
+    }
+}
---
+++ Tutorial work/Week 9/Week10Program1/Week10Program1/EmployeeList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Week10Program1
+{
+    class EmployeeList
+    {
+        private List<Employee> employees;
+
+        // EmployeeList constructor
+        public EmployeeList()
+        {
+            employees = new List<Employee>();
+        }
+
+        // Method to load all employees from the given file
+        public void LoadEmployees(string filename)
+        {
+            StreamReader fileContent = new StreamReader(filename);
+
+            // Read the StremReader till the last line
+            while (!fileContent.EndOfStream)                                                                                                                                                                                                             
+            {
+                Employee employee = new Employee();
+
+                // Read each line and from the StreamReader
+                string line = fileContent.ReadLine();
+
+                // Load the employee detail from file to respective fields
+                employee.LoadEmployee(line);
+
+                // Add the details to the list collection
+                employees.Add(employee);
+            }
+
+            fileContent.Close();
+        }
+
+        // Method to display the details for all the Employee
+        public void PrintEmployees()
+        {
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.ToString());
+            }
+        }
+        //  Sort the employees by Employee ID
+        public void SortEmployees()
+        {
+            employees.Sort();
+        }
+    }
+}
